25300ac7e00d60b44f00722b6e5372ab21a49563
mrbenxb <yztevenson.pogi@gmail.com>
2026-07-10 19:09:59 +0800

    style: load persistent tactic dock

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './contra-race.css'
 import './tactic-gauge.css'
+import './free-tactic.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
