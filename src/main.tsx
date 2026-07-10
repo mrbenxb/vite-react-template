84cdbd6433402a22cbbe156aaec03f754a57add9
mrbenxb <yztevenson.pogi@gmail.com>
2026-07-10 19:17:54 +0800

    style: load motion synchronization cues

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './contra-race.css'
 import './tactic-gauge.css'
 import './free-tactic.css'
+import './motion-sync.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
