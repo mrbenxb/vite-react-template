ef3401bd60f8a87ba5bb390a4753ced2276d9e3b
mrbenxb <yztevenson.pogi@gmail.com>
2026-07-10 18:06:18 +0800

    style: load Contra-style race presentation

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './side-race.css'
+import './contra-race.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
